5a2edf499ae9516171197624671afc6402ee696d
NiallD565 <niall.d565@gmail.com>
2019-03-26 17:48:19 +0000

    Error handling

--- padfile.c
+++ padfile.c
@@ -22,40 +22,49 @@ int main(int argc, char *argv[]){
 	f = fopen(argv[1], "r");
 
 	int i;
-	
-	while(S == READ) {
-		nobytes = fread(M.e, 1, 64, f);
-		printf("Read %2llu bytes\n", nobytes);
-		// Multiply number of bytes read by 8
-		nobits = nobits + (nobytes * 8);
-		if (nobytes < 56) {
-			printf("I've found a block with less than 55 bytes!\n");
-			// 0x80 hexidecimal value for 1 followed by 7 0s
-			M.e[nobytes] = 0x80;
-			while (nobytes < 56) {
-				nobytes = nobytes + 1;
-				M.e[nobytes] = 0x00;
-			}
-			// Last 8 are for number of bytes in the files
-			M.s[7] = nobits;
-			S = FINISH;
+	if (NULL != f){
+		fseek (f,0,SEEK_END);
+		int size = ftell(f);
+
+		if(0 == size)
+			printf("file is empty\n");
+
+		else if (size > 0)
+			fseek (f, 0, SEEK_SET);	
+		while(S == READ) {
+			nobytes = fread(M.e, 1, 64, f);
+			printf("Read %2llu bytes\n", nobytes);
+			// Multiply number of bytes read by 8
+			nobits = nobits + (nobytes * 8);
+				if (nobytes < 56) {
+					printf("I've found a block with less than 55 bytes!\n");
+					// 0x80 hexidecimal value for 1 followed by 7 0s
+					M.e[nobytes] = 0x80;
+					while (nobytes < 56) {
+						nobytes = nobytes + 1;
+						M.e[nobytes] = 0x00;
+					}
+					// Last 8 are for number of bytes in the files
+					M.s[7] = nobits;
+					S = FINISH;
+				}
+				// If there isn't enough room for the bytes in the last block
+				 else if (nobytes < 64){
+					S = PAD0;
+					M.e[nobytes] = 0x80;
+					// Fill the file with 0s until the last 64
+					while (nobytes < 64){
+						nobytes = nobytes + 1;
+						M.e[nobytes] = 0x00;
+					}
+				}
+				// If there are exactly 64 bytes remaining
+				else if (feof(f)) {
+					S = PAD1;
+				}
 		}
-		// If there isn't enough room for the bytes in the last block
-		 else if (nobytes < 64){
-			S = PAD0;
-			M.e[nobytes] = 0x80;
-			// Fill the file with 0s until the last 64
-			while (nobytes < 64){
-				nobytes = nobytes + 1;
-				M.e[nobytes] = 0x00;
-			}
-		}
-		// If there are exactly 64 bytes remaining
-		else if (feof(f)) {
-			S = PAD1;
-		}
-		
-	}
+	}	
+
 	if (S == PAD0 || S == PAD1){
 		// Leave 8 bytes for 64bit integer
 		for(i = 0; i < 56; i++)
