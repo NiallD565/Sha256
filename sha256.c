09354563000edeb90b2fd39f914ba76d3f6b9dfe
NiallD565 <niall.d565@gmail.com>
2019-03-27 20:05:13 +0000

    Merge padding to sha256

--- sha256.c
+++ sha256.c
@@ -4,7 +4,18 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void sha256();
+// Chunk in memory for message block in different sizes
+union msgblock {
+	uint8_t  e[64];
+	uint32_t t[16];
+	uint64_t s[8];
+};
+
+// A flag for where we are in the file
+enum status {READ, PAD0, PAD1, FINISH};
+
+// Calculates sha256 of the file
+void sha256(FILE *f);
 
 // See Sections 4.1.2 and 4.2.2
 uint32_t sig0(uint32_t x);
@@ -18,8 +29,12 @@ uint32_t shr(uint32_t n, uint32_t x);
 uint32_t SIG0(uint32_t x);
 uint32_t SIG1(uint32_t x);
 
+// See Section 4.1.2
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
 uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
+
+// Retrieves the next message block
+int nextmsgblock(FILE *f, union msgblock *M, enum status *S, uint64_t *nobits);
 
 // ======== Macros ========
 #define SWAP_UINT32(x) (((x) >> 24) | (((x) & 0x00FF0000) >> 8) | (((x) & 0x0000FF00) << 8) | ((x) << 24))
@@ -27,14 +42,30 @@ uint32_t Maj(uint32_t x, uint32_t y, uint32_t z);
 
 
 int main(int argc, char *argv[]){
+	// Open the file given as first command line arguement
+	FILE* msgf;
+	msgf = fopen(argv[1], "r");
+
+	// Run the secure hash algorithm on the file
+	sha256(msgf);
 	
-	sha256();
+	// Close the file	
+	fclose(msgf);
 
 	return 0;
 }
 
-void sha256(){
-	
+void sha256(FILE *msgf){
+
+	// The current message block
+	union msgblock M;
+
+	// Number of bits read from the file
+	uint64_t nobits = 0;
+
+	// The status of the message blocks, in terms of padding.
+	enum status S = READ;
+
 	// The K constants
 	// See Section 4.2.2
 	uint32_t K[] = {
@@ -75,68 +106,67 @@ void sha256(){
 	};
 
 	// Current message block
-	uint32_t M[16] = {0, 0, 0, 0, 0, 0, 0, 0,};
+	//uint32_t M[16] = {0, 0, 0, 0, 0, 0, 0, 0,};
 
 	// For Looping.
 	int t;
 	int i;
 	
 	// Loop through message blocks as per page 22
-	for (i = 0; i < 1; i++){
+	while (nextmsgblock(msgf, &M, &S, &nobits)) {
 
-	// From page 22, W[t] = M[t] for 0 <= t <= 15
-	for (t = 0; t < 16; t++)
-		W[t] = M[t];
+		// From page 22, W[t] = M[t] for 0 <= t <= 15
+		for (t = 0; t < 16; t++)
+			W[t] = M.t[t];
 
-	// From page 22, W[t] ...
-	for(t = 16; t <64; t++)
-		W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
+		// From page 22, W[t] ...
+		for(t = 16; t <64; t++)
+			W[t] = sig1(W[t-2]) + W[t-7] + sig0(W[t-15]) + W[t-16];
 
-	// Initialise a, b, c, d, e, f, g, h. Step 2 Pg.22
-	a = H[0];
-	b = H[1];
-	c = H[2];
-	d = H[3];
-	e = H[4];
-	f = H[5];
-	g = H[6];
-	h = H[7];
+		// Initialise a, b, c, d, e, f, g, h. Step 2 Pg.22
+		a = H[0];
+		b = H[1];
+		c = H[2];
+		d = H[3];
+		e = H[4];
+		f = H[5];
+		g = H[6];
+		h = H[7];
+	
+		for (t = 0; t < 64; t++) {
+			T1 = h + SIG1(e) + Ch(e, f, g) + K[t] + W[t];
+			T2 = SIG0(a) + Maj(a, b, c);
+			h = g;
+			g = f;
+			f = e;
+			e = d + T1;
+			d = c;
+			c = b;
+			b = a;
+			a = T1 + T2;
+		}
 
-	for (t = 0; t < 64; t++) {
-		T1 = h + SIG1(e) + Ch(e, f, g) + K[t] + W[t];
-		T2 = SIG0(a) + Maj(a, b, c);
-		h = g;
-		g = f;
-		f = e;
-		e = d + T1;
-		d = c;
-		c = b;
-		b = a;
-		a = T1 + T2;
-	}
-
-
-	H[0] = a + H[0];
-	H[1] = b = H[1];
-	H[2] = c = H[2];
-	H[3] = d = H[3];
-	H[4] = e = H[4];
-	H[5] = f = H[5];
-	H[6] = g = H[6];
-	H[7] = h = H[7];
+		H[0] = a + H[0];
+		H[1] = b = H[1];
+		H[2] = c = H[2];
+		H[3] = d = H[3];
+		H[4] = e = H[4];
+		H[5] = f = H[5];
+		H[6] = g = H[6];
+		H[7] = h = H[7];
 
 	}
 	// Check if it is big endian if it isn't bytes are swapped
 	if(IS_BIG_ENDIAN){
-		printf("%x %x %x %x %x %x %x %x\n", H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]);
+		printf("%08x %08x %08x %08x %08x %08x %08x %08x\n", H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]);
 
 	}
 	else{
-		printf("%x %x %x %x %x %x %x %x\n", SWAP_UINT32(H[0]), SWAP_UINT32(H[1]), SWAP_UINT32(H[2]), SWAP_UINT32(H[3]), SWAP_UINT32(H[4]), SWAP_UINT32(H[5]), SWAP_UINT32(H[6]), SWAP_UINT32(H[7]));
+		printf("%08x %08x %08x %08x %08x %08x %08x %08x\n", SWAP_UINT32(H[0]), SWAP_UINT32(H[1]), SWAP_UINT32(H[2]), SWAP_UINT32(H[3]), SWAP_UINT32(H[4]), SWAP_UINT32(H[5]), SWAP_UINT32(H[6]), SWAP_UINT32(H[7]));
 	}
 
 }
-
+// See Section 3.2 for definitions
 uint32_t rotr(uint32_t n, uint32_t x){
 	return (x >> n) | (x << (32 - n));
 }
@@ -167,4 +197,83 @@ uint32_t Ch(uint32_t x, uint32_t y, uint32_t z){
 }
 uint32_t Maj(uint32_t x, uint32_t y, uint32_t z){
 	return ((x & y) ^ (x & z) ^ (y & z));
+}
+
+int nextmsgblock(FILE *msgf, union msgblock *M, enum status *S, uint64_t *nobits){
+	// Number of bytes gotten from fREAD
+	uint64_t nobytes;
+	 	
+	int i;
+/*	
+	if (NULL != msgf){
+		fseek (msgf,0,SEEK_END);
+		int size = ftell(msgf);
+		if(0 == size)
+			printf("file is empty\n");
+		else if (size > 0)
+			fseek (msgf, 0, SEEK_SET);
+
+		*/	
+//======================================================
+		//If message blocks are done S = FINISH
+		if(*S == FINISH)
+			return 0;
+// =====================================================
+
+	       // OTHERWISE CHECK IF WE NEED ANOTHER BLOCK OF PADDING	
+		if (*S == PAD0 || *S == PAD1){
+			// Leave 8 bytes for 64bit integer
+			for(i = 0; i < 56; i++)
+				M->e[i] = 0x00;
+			// Set last 64 bits to number of bits in the file
+			M->s[7] = *nobits;
+			// Tell S we are finished
+			*S = FINISH;
+			// If S was PAD1, then set the first bits of M to 1
+			if (*S == PAD1)
+				M->e[0] = 0x80;
+			// Keep the loop going for 1 more iteration
+			return 1;
+		}
+		
+			// Haven't finished reading the files (S == READ)
+			nobytes = fread(M->e, 1, 64, msgf);
+		
+			// Multiply number of bytes read by 8
+			*nobits = *nobits + (nobytes * 8);
+				// if less than 56 bytes we can pad the whole block
+				if (nobytes < 56) {
+					// 0x80 hexidecimal value for 1 followed by 7 0s
+					// Add 1 bit per standard 
+					M->e[nobytes] = 0x80;
+					// Add zero bits until the last 64 bits
+					while (nobytes < 56) {
+						nobytes = nobytes + 1;
+						M->e[nobytes] = 0x00;
+					}
+					// Last 8 are for number of bytes in the files
+					M->s[7] = *nobits;
+					*S = FINISH;
+				}
+				// If there isn't enough room for the bytes in the last block
+				else if (nobytes < 64){
+					// Another blocked needed with padding but no 1 bits
+					*S = PAD0;
+					// Put one bit into current block
+					M->e[nobytes] = 0x80;
+					// Fill the file with 0s until the last 64
+					while (nobytes < 64){
+						nobytes = nobytes + 1;
+						M->e[nobytes] = 0x00;
+					}
+				}
+				// If there are exactly 64 bytes remaining
+				else if (feof(msgf)) {
+					// Message block with all padding needed
+					*S = PAD1;
+				}
+//		}	
+
+	// Return 1 so the the function is called again
+	return 1;
 }
